af9212c35dc8491c7db499fdf3c0689fabf7aefb
Pablo Tato Ramos <pabtatoramos@gmail.com>
2022-08-07 09:03:37 +0200

    "Enable" tests

--- source/test/interpreter.zig
+++ source/test/interpreter.zig
@@ -3,24 +3,27 @@ const bytecode = @import("../bytecode.zig");
 const runInterpreterTest = @import("util.zig").runInterpreterTest;
 
 test "toosimple" {
+    if (true) return error.SkipZigTest;
     const program = "1 + 1";
     var result = try runInterpreterTest(program);
     defer result.deinit();
-    // try std.testing.expectEqualSlices(i64, &.{2}, result.stack);
+    try std.testing.expectEqualSlices(i64, &.{2}, result.stack);
 }
 
 test "aparen" {
+    if (true) return error.SkipZigTest;
     const program = "(1 + 1)";
     var result = try runInterpreterTest(program);
     defer result.deinit();
-    // try std.testing.expectEqualSlices(i64, &.{2}, result.stack);
+    try std.testing.expectEqualSlices(i64, &.{2}, result.stack);
 }
 
 test "hello" {
+    if (true) return error.SkipZigTest;
     const program = "(1 + (111 - 3)) * (2 / 1)";
     var result = try runInterpreterTest(program);
     defer result.deinit();
-    // try std.testing.expectEqualSlices(i64, &.{218}, result.stack);
+    try std.testing.expectEqualSlices(i64, &.{218}, result.stack);
 }
 
 test "more" {
@@ -30,7 +33,7 @@ test "more" {
     ;
     var result = try runInterpreterTest(program);
     defer result.deinit();
-    
+
     const expected =
         \\11
         \\77
